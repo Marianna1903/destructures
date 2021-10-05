8799e5070ae8b2f1fbe9f64de1c9f9bddef404ee
Marianna1903 <84705083+Marianna1903@users.noreply.github.com>
2021-10-05 14:36:17 +0400

    Add files via upload

---
+++ destruct.js
@@ -0,0 +1,21 @@
+// let data = [1,2,3];
+// let [a,,b,c=4]=data;
+// console.log(a,b,c)
+
+// let obj = {
+// 	f1:"v1",
+// 	f2:2,
+// 	f3:"v3",
+// }
+// let {f1="v1",f2:field2,f4="v4"}=obj;
+// console.log(f1,field2,f4);
+
+// let girq = {
+// 	anun: "Haxtakan kamar",
+// 	hexinak: "Remark",
+// 	janr: "paterazmakan vep",
+// 	hratarakum: "17/06/1945",
+// }
+// let {anun:booktitle,hexinak:bookauthor}=girq;
+// document.write(booktitle+" girqy grvel e "+bookauthor+"-i koxmic");
+
